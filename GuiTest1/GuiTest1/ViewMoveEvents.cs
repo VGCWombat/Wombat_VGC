f02ebe335d9c77a4a29f297ba11cb9d4b4efd633
zasoniak <mateusz.zasonski@gmail.com>
2014-04-25 14:21:44 +0200

    qa

---
+++ GuiTest1/GuiTest1/ViewMoveEvents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GuiTest1
+{
+    class ViewMoveEvents: ObservableCollection<MoveEvent>
+    {
+        public static Random random = new Random();
+        public ViewMoveEvents(): base()
+        {
+            Add(new MoveEvent(1, new Point(random.Next(0, 800), 0), random.Next(20, 200)));
+            Add(new MoveEvent(2, new Point(random.Next(0, 800), 0), random.Next(20, 200)));
+            Add(new MoveEvent(3, new Point(random.Next(0, 800), 0), random.Next(20, 200)));
+            Add(new MoveEvent(4, new Point(random.Next(0, 800), 0), random.Next(20, 200)));
+            Add(new MoveEvent(5, new Point(random.Next(0, 800), 0), random.Next(20, 200)));
+            Add(new MoveEvent(6, new Point(random.Next(0, 800), 0), random.Next(20, 200)));
+        } 
+    }
+}
