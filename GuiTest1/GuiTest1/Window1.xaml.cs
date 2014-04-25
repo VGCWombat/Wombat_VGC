9ad9086604bf62d2f9c051a88bd886b84def8444
Łukasz Dzwoniarski <lukaszdzwoniarski@gmail.com>
2014-04-25 13:55:09 +0200

    ja

--- GuiTest1/GuiTest1/Window1.xaml.cs
+++ GuiTest1/GuiTest1/Window1.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+using System.Drawing;
+using System.Windows.Forms;
+ 
 
 namespace GuiTest1
 {
@@ -19,9 +24,68 @@ namespace GuiTest1
     /// </summary>
     public partial class Window1 : Window
     {
-        public Window1()
+        
+        
+        public Window1(int dimX, int dimY)
         {
             InitializeComponent();
+            System.Drawing.Rectangle workingRectangle =
+            Screen.PrimaryScreen.WorkingArea;
+            
+
+            double sze = this.ActualWidth;
+            System.Windows.Size max = new System.Windows.Size(workingRectangle.Width,workingRectangle.Height-100);
+            
+            canvas.Measure(max);
+            canvas.Arrange(new Rect(0, 0, max.Width, max.Height));
+            
+            
+            System.Windows.Shapes.Rectangle[,] myRectangle = new System.Windows.Shapes.Rectangle[50,50];
+            double startUp=0;
+            int col = (int)(dimX - (dimX % 1.2) / 1.2);
+            int row = (int)(dimY - (dimY % 1.2) / 1.2);
+            int wi=(int)((float)dimX / dimY * canvas.ActualHeight);
+            int roz = (int)(canvas.ActualWidth - wi)/2;
+            //int centerX = canvas.ActualWidth - col * 30;
+            //start = centerX;
+            double start = (roz);
+            double wysokosc = canvas.ActualHeight;
+            double szerokosc = canvas.ActualWidth;
+            
+            Add.Content = startUp;
+            Delete.Content = canvas.ActualWidth;
+           
+
+            for(int i=0;i<row;i++)
+
+            {   
+                for (int j = 0; j < col; j++)
+                {
+                    myRectangle[i, j] = new System.Windows.Shapes.Rectangle();
+                    myRectangle[i,j].Width = (canvas.ActualHeight/row)-2;
+                    myRectangle[i, j].Height = (canvas.ActualHeight / row) - 2;
+                    SolidColorBrush myBrush = new SolidColorBrush(Colors.Green);
+                    myRectangle[i,j].Stroke = System.Windows.Media.Brushes.Red;
+                    myRectangle[i,j].StrokeThickness = 4;
+                    myRectangle[i,j].Fill = myBrush;
+                    canvas.Children.Insert(0, myRectangle[i, j]);
+                    Canvas.SetLeft(myRectangle[i, j], start);
+                    Canvas.SetTop(myRectangle[i, j], startUp);
+                    start = start + myRectangle[i, j].Width+2;
+                }
+                startUp = startUp + (canvas.ActualHeight / row) -  2;
+                start = (roz);
+            }
+            
         }
+
+
+
+
+
+
+        public static double ActualHeightPropertyHeight { get; set; }
+
+        public static double ActualWidthPropertyWidth { get; set; }
     }
 }
---
+++ GuiTest1/GuiTest1/Window_dimension.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuiTest1
+{
+    /// <summary>
+    /// Interaction logic for Window_dimension.xaml
+    /// </summary>
+    public partial class Window_dimension : Window
+    {
+        
+        public Window_dimension()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            int valX = Convert.ToInt16(dimensionX.Text);
+            int valY = Convert.ToInt16(dimensionY.Text);
+            Window1 creator = new Window1(valX,valY);
+            creator.Show();
+            
+            
+        }
+    }
+}
